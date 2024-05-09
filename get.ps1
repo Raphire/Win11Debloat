f33125554cb393da13a16d68f974c7d215b0503a
loadstring1 <156520308+loadstring1@users.noreply.github.com>
2024-05-09 22:13:47 +0200

    Add files via upload

---
+++ get.ps1
@@ -0,0 +1,19 @@
+#Requires -RunAsAdministrator
+
+$Script:showWindowAsync = Add-Type -MemberDefinition @"
+[DllImport("user32.dll")]
+public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+"@ -Name "Win32ShowWindowAsync" -Namespace Win32Functions -PassThru
+
+Function Hide-Powershell()
+{
+$null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 2)
+}
+
+cd $env:TEMP 
+winget install git.git
+git clone https://github.com/Raphire/Win11Debloat/ 
+Hide-Powershell
+Start-Process .\Win11Debloat\Run.bat -Wait
+Remove-Item -LiteralPath "Win11Debloat" -Force -Recurse
+Get-Process -Id $pid | Stop-Process

44703765fbba04ea62e7097da47de5545b2bd291
TPAXcc <91980905+TPAXcc@users.noreply.github.com>
2025-05-22 03:57:40 +0800

    Support launching script through Windows Terminal if installed (#225)

--- Run.bat
+++ Run.bat
@@ -1,1 +1,14 @@
-PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs}"
+@echo off
+:: Set Windows Terminal installation paths. (Default and Scoop installation)
+set "wtDefaultPath=%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe"
+set "wtScoopPath=%USERPROFILE%\scoop\apps\windows-terminal\current\wt.exe"
+
+:: Launch the script in Windows Terminal if installed, otherwise use default PowerShell.
+if exist "%wtDefaultPath%" (
+    PowerShell -Command "Start-Process -FilePath '%wtDefaultPath%' -ArgumentList 'PowerShell -NoProfile -ExecutionPolicy Bypass -File \"\"%~dp0Win11Debloat.ps1\"\"' -Verb RunAs"
+) else if exist "%wtScoopPath%" (
+    PowerShell -Command "Start-Process -FilePath '%wtScoopPath%' -ArgumentList 'PowerShell -NoProfile -ExecutionPolicy Bypass -File \"\"%~dp0Win11Debloat.ps1\"\"' -Verb RunAs"
+) else (
+    echo Windows Terminal not found, using default PowerShell...
+    PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"\"%~dp0Win11Debloat.ps1\"\"' -Verb RunAs}"
+)

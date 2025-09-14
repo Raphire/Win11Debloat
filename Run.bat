a8127c9074592f0b8207c3538f59ea281dafa918
tylerhekman <tylerhekman@users.noreply.github.com>
2025-09-14 00:47:39 -0700

    escaped double quotes causes incorrect string parsing for path with spaces

--- Run.bat
+++ Run.bat
@@ -5,10 +5,10 @@ set "wtScoopPath=%USERPROFILE%\scoop\apps\windows-terminal\current\wt.exe"
 
 :: Launch the script in Windows Terminal if installed, otherwise use default PowerShell.
 if exist "%wtDefaultPath%" (
-    PowerShell -Command "Start-Process -FilePath '%wtDefaultPath%' -ArgumentList 'PowerShell -NoProfile -ExecutionPolicy Bypass -File \"\"%~dp0Win11Debloat.ps1\"\"' -Verb RunAs"
+    PowerShell -Command "Start-Process -FilePath '%wtDefaultPath%' -ArgumentList 'PowerShell -NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs"
 ) else if exist "%wtScoopPath%" (
-    PowerShell -Command "Start-Process -FilePath '%wtScoopPath%' -ArgumentList 'PowerShell -NoProfile -ExecutionPolicy Bypass -File \"\"%~dp0Win11Debloat.ps1\"\"' -Verb RunAs"
+    PowerShell -Command "Start-Process -FilePath '%wtScoopPath%' -ArgumentList 'PowerShell -NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs"
 ) else (
     echo Windows Terminal not found, using default PowerShell...
-    PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"\"%~dp0Win11Debloat.ps1\"\"' -Verb RunAs}"
+    PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs}"
 )

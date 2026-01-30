47271ac1e4405d0ca5476c203a8137afbc13b57b
lucas-timeworkstudio <lvblue0022@gmail.com>
2026-01-29 19:05:20 -0500

    add error logging to run.bat

--- Run.bat
+++ Run.bat
@@ -3,12 +3,38 @@
 set "wtDefaultPath=%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe"
 set "wtScoopPath=%USERPROFILE%\scoop\apps\windows-terminal\current\wt.exe"
 
-:: Launch the script in Windows Terminal if installed, otherwise use default PowerShell.
+:: Set the log file path
+set "logFile=%~dp0Win11Debloat.log"
+
+:: Check if Windows Terminal is installed
 if exist "%wtDefaultPath%" (
-    PowerShell -Command "Start-Process -FilePath '%wtDefaultPath%' -ArgumentList 'PowerShell -NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs"
+    set "wtPath=%wtDefaultPath%"
 ) else if exist "%wtScoopPath%" (
-    PowerShell -Command "Start-Process -FilePath '%wtScoopPath%' -ArgumentList 'PowerShell -NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs"
+    set "wtPath=%wtScoopPath%"
 ) else (
-    echo Windows Terminal not found, using default PowerShell...
-    PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs}"
+    echo Windows Terminal not found. Using default PowerShell instead.
+    set "wtPath="
+)
+
+:: Launch the script in Windows Terminal if installed, otherwise use default PowerShell
+if defined wtPath (
+    PowerShell -Command "Start-Process -FilePath '%wtPath%' -ArgumentList 'PowerShell -NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs" >> "%logFile%"
+) else (
+    echo Windows Terminal not found. Using default PowerShell instead...
+    PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs}" >> "%logFile%"
+)
+
+:: Check if the script has finished running
+:checkScript
+if exist "%~dp0Win11Debloat.ps1" (
+    ping -n 1 127.0.0.1 >nul
+    if errorlevel 1 (
+        echo The script has finished running. >> "%logFile%"
+    ) else (
+        echo The script is still successfully running, Rechecking in 10 seconds... >> "%logFile%"
+        timeout /t 10 /nobreak >nul
+        goto :checkScript
+    )
+) else (
+    echo The script has finished running. >> "%logFile%"
 )

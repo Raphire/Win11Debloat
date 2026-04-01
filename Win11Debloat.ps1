cbd61902edb04b83eeee69c7fd983c9ec74bf12f
Raphire <9938813+Raphire@users.noreply.github.com>
2026-04-02 00:03:26 +0200

    Set default launch mode to CLI for deployment-targeted parameters

--- Win11Debloat.ps1
+++ Win11Debloat.ps1
@@ -361,6 +361,9 @@ if ((Test-Path $script:SavedSettingsFilePath) -and ([String]::IsNullOrWhiteSpace
     Remove-Item -Path $script:SavedSettingsFilePath -recurse
 }
 
+# Default to CLI mode for deployment-targeted parameters.
+$launchInCLI = $CLI -or $script:Params.ContainsKey("User") -or $script:Params.ContainsKey("Sysprep") -or $script:Params.ContainsKey("AppRemovalTarget")
+
 # Only run the app selection form if the 'RunAppsListGenerator' parameter was passed to the script
 if ($RunAppsListGenerator) {
     PrintHeader "Custom Apps List Generator"
@@ -409,7 +412,7 @@ if ((-not $script:Params.Count) -or $RunDefaults -or $RunDefaultsLite -or $RunSa
         }
     }
     else {
-        if ($CLI) {
+        if ($launchInCLI) {
             $Mode = ShowCLIMenuOptions 
         }
         else {

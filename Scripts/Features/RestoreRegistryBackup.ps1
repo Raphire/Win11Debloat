4e9df9c2fbe2da5d0b5b84142520d83bd4237d25
Jeffrey <9938813+Raphire@users.noreply.github.com>
2026-04-28 21:21:38 +0200

    fix restore of HKEY_CURRENT_USERS\Control Panel\Desktop

--- Scripts/Features/RestoreRegistryBackup.ps1
+++ Scripts/Features/RestoreRegistryBackup.ps1
@@ -327,12 +327,12 @@ function Convert-BackupDataToByteArray {
     }
 
     if ($Data -is [byte[]]) {
-        return $Data
+        return ,$Data
     }
 
     $items = @($Data)
     if ($items.Count -eq 0) {
-        return (New-Object byte[] 0)
+        return ,(New-Object byte[] 0)
     }
 
     foreach ($item in $items) {
@@ -355,7 +355,7 @@ function Convert-BackupDataToByteArray {
         $bytes[$i] = [byte][int]$items[$i]
     }
 
-    return $bytes
+    return ,$bytes
 }
 
 function Remove-RegistrySubKeyTreeIfExists {
